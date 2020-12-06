23114d4abeb4d05ffc2c5d7716d9d175d65bed86
nucbaixu <nuc_baixu@163.com>
2020-12-06 17:43:14 +0800

    添加了lambda函数的使用说明

---
+++ ChangeLenTemplate/ChangeLenTemplate.cpp
@@ -0,0 +1,115 @@
+﻿#include <stdexcept>
+#include <iostream>
+#include <stdarg.h>
+
+using namespace std;
+
+/******************************************************
+ @ function : 	可变长参数的函数，有局限性，请思考有何局限
+ @ Access 	:	
+ @ Parameter:	
+ @ Returns  :	
+ @ Remarks 	:   
+ @ author   :   
+******************************************************/
+double SumOfFloat(int count, ...) 
+{
+	va_list ap;
+	double sum = 0;
+	va_start(ap, count);//获得变长列表的句柄ap
+	
+	for (int i = 0; i < count; i++)
+		sum += va_arg(ap, double);//每次获得一个参数
+	va_end(ap);
+	
+	return sum;
+}
+
+
+//可变参数模板函数的定义
+template<class ... T> void func(T ... args)
+{
+	//可变参数模板函数
+	//sizeof...（sizeof后面有3个小点）计算变参个数
+	cout << "num = " << sizeof...(args) << endl;
+}
+
+
+//递归终止函数
+void debug()
+{
+	cout << "empty\n";
+}
+
+//展开函数
+template <class T, class ... Args>
+void debug(T first, Args ... last)
+{
+	cout << "parameter " << first << endl;
+	debug(last...);
+}
+
+
+template <class T>
+void print(T arg)
+{
+	cout << arg << endl;
+}
+
+template <class ... Args>
+void expand(Args ... args)
+{
+	int a[] = { (print(args), 0)... };
+}
+
+
+template<typename... A> class BMW {};  // 变长模板的声明
+
+template<typename Head, typename... Tail>  // 递归的偏特化定义
+class BMW<Head, Tail...> : public BMW<Tail...>
+{
+	//当实例化对象时，则会引起基类的递归构造
+public:
+	BMW()
+	{
+		printf("type: %s\n", typeid(Head).name());
+	}
+
+	Head head;
+};
+
+template<> class BMW<> {};  // 边界条件
+
+
+template <long... nums> struct Multiply;// 变长模板的声明
+
+template <long first, long... last>
+struct Multiply<first, last...> // 变长模板类
+{
+	static const long val = first * Multiply<last...>::val;
+};
+
+template<>
+struct Multiply<> // 边界条件
+{
+	static const long val = 1;
+};
+
+
+
+int main() 
+{
+	printf("%f\n", SumOfFloat(3, 1.2f, 3.4, 5.6));//10.200000
+
+	func();     // num = 0
+	func(1);    // num = 1
+	func(2, 1.0);   // num = 2
+
+	debug(1, 2, 3, 4);
+
+	expand(1, 2, 3, 4);
+
+	BMW<int, char, float> car;
+
+	cout << Multiply<2, 3, 4, 5>::val << endl; // 120
+}
---
+++ Lambda/Lambda.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "time.h"
+using namespace std;
+
+void main()
+{
+	int a = 20, b = 10;
+
+	auto totalAB = [](int x, int y)->int { return x + y; };
+	int aAddb = totalAB(a, b);
+	cout << "aAddb :" << aAddb << endl;
+
+	auto totalAB2 = [a, &b]()->int { return a + b; };
+	int aAddb2 = totalAB2();
+	cout << "aAddb2 :" << aAddb2 << endl;
+
+	auto totalAB3 = [=]()->int { return a + b; };
+	int aAddb3 = totalAB3();
+	cout << "aAddb3 :" << aAddb3 << endl;
+
+	[] {}; // 最简lambda函数
+	[=] { return a + b; }; // 省略了参数列表与返回类型，返回类型由编译器推断为int
+	auto fun1 = [&](int c) { b = a + c; }; // 省略了返回类型，无返回值
+	auto fun2 = [=, &b](int c)->int { return b += a + c; }; // 各部分都很完整的lambda函数
+	cout << "fun2(100) :" << fun2(100) << endl;
+
+
+	vector<int> nVec;
+	for (int i = 0; i < 100000; ++i)
+	{
+		nVec.push_back(i);
+	}
+
+	double time_Start = (double)clock();
+	for (vector<int>::const_iterator it = nVec.begin(); it != nVec.end(); ++it)
+	{
+		cout << *it << endl;
+	}
+	
+	double time_Finish = (double)clock();
+	double time_Interval_1 = (double)(time_Finish - time_Start) / 1000;
+
+	time_Start = (double)clock();
+	for_each(nVec.begin(), nVec.end(), [](int val) { cout << val << endl; });
+	time_Finish = (double)clock();
+	double time_Interval_2 = (double)(time_Finish - time_Start) / 1000;
+
+	cout << "time_Interval_1 :" << time_Interval_1 << endl;
+	cout << "time_Interval_2 :" << time_Interval_2 << endl;
+
+}
